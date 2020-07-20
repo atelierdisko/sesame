f592dd3d8732eb1b05cb8d9763253a5dc47e2790
schaschjan <jan-jakub@atelierdisko.de>
2020-07-20 12:16:40 +0200

    add copyright, cleanup docs

--- src/helpers/generatePassphrase.js
+++ src/helpers/generatePassphrase.js
@@ -1,15 +1,3 @@
-/*
- * Copyright 2020 Atelier Disko. All rights reserved.
- *
- * Use of this source code is governed by the AD General Software
- * License v1 that can be found under https://atelierdisko.de/licenses
- *
- * This software is proprietary and confidential. Redistribution
- * not permitted. Unless required by applicable law or agreed to
- * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
-
 const generatePassphrase = (length = 8) => {
   function number() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
